121dd04c550c92bc3402a6b2300eb4f5a05e4c52
krn <contadorkrn@gmail.com>
2024-02-15 21:18:45 -0300

    Added: included sidebar for file uploads / Changed: imported PIL module to be able to rotate images

--- src/app.py
+++ src/app.py
@@ -1,5 +1,30 @@
 import streamlit as st
-import glitch_this as gt
+from glitch_this import ImageGlitcher
+from PIL import Image, ImageOps
+
+glitcher = ImageGlitcher()
+file_ext = [".jpg", ".jpeg", ".png", ".gif"]
 
 with st.sidebar:
-    pass
+    uploaded_images = st.file_uploader(
+        label="Upload your images here",
+        type=file_ext,
+        label_visibility="hidden",
+        accept_multiple_files=True,
+    )
+
+    if uploaded_images:
+        # Include glitcher config
+        pass
+
+    else:
+        st.write("Please upload your images above.")
+
+if uploaded_images:
+    for img in uploaded_images:
+        img = Image.open(img)
+        img = ImageOps.exif_transpose(img)
+        st.image(image=img)
+
+else:
+    st.write("Waiting for images...")
