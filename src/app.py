33651ab4eddbc877f2d50249243db3c375f08c33
krn <contadorkrn@gmail.com>
2024-02-17 11:45:09 -0300

    Added: settings to control the glitching / Changed: made the expander to always start expanded

--- src/app.py
+++ src/app.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageOps
 glitcher = ImageGlitcher()
 file_ext = [".jpg", ".jpeg", ".png", ".gif"]
 
+
 with st.sidebar:
     uploaded_images = st.file_uploader(
         label="Upload your images here",
@@ -17,11 +18,15 @@ with st.sidebar:
         st.markdown("---")
         st.write("Use the settings bellow to glith your images:")
 
+        glitch_amount = st.slider(label="Glitch Amount", min_value=0.1, max_value=10.0)
+        color_offset = st.toggle(label="Color Offset", value=True)
+        scan_lines = st.toggle(label="Scan Lines", value=False)
+
     else:
         st.write("Please upload your images above.")
 
 if uploaded_images:
-    with st.expander(label="Uploaded Images:"):
+    with st.expander(label="Uploaded Images:", expanded=True):
         img_list = []
         for img in uploaded_images:
             img = Image.open(img)
